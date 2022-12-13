9d5574f49b0c1f8d14bc522eff05c712cc1cc850
Etienne Millon <me@emillon.org>
2022-12-13 10:24:09 +0100

    Fix test

--- readme.ml
+++ readme.ml
@@ -1,10 +1,15 @@
 open Base
 open Stdio
 
+let file_is_not_empty path =
+  match In_channel.with_file path ~f:In_channel.input_all with
+  | "" -> false
+  | _ -> true
+  | exception Sys_error _ -> false
+
 let emoji_for_day day =
   let path i = Printf.sprintf "day%02d/p%d.expected" day i in
-  if Caml.Sys.file_exists (path 1) && Caml.Sys.file_exists (path 2) then
-    ":star2:"
+  if file_is_not_empty (path 1) && file_is_not_empty (path 2) then ":star2:"
   else ":snowflake:"
 
 let () =
